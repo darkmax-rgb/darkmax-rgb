19fbc8cc3f6ec33f9ebab935ca29e38d0742ddeb
darkmax-rgb <avishekmitra27@gmail.com>
2026-04-21 21:33:44 +0530

    requirements.py

--- requirements.py
+++ requirements.py
@@ -3,3 +3,4 @@ pip install python-telegram-bot
 pip3 install python-telegram-bot
 pip install python-telegram-bot pyTelegramBotAPI requests aiohttp httpx
 pip install requests
+pip3 install requests
